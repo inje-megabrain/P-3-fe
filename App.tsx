1aa8c814039e29d258c986d3d031e1fc0dd01be1
narinn-star <skfls2618@naver.com>
2023-01-07 22:10:28 +0900

    chore : styled-components error, navigation 적용
    babel-styled-components 오류 수정
    NavigationContainer, createNativeStackNavigator 사용

--- App.tsx
+++ App.tsx
@@ -7,17 +7,34 @@ import {
 	View,
 } from "react-native";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./src/Login";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.main}>P-3 Project {":)"}</Text>
-			<TouchableOpacity>
-				<Text>안뇽</Text>
-			</TouchableOpacity>
-			<TextInput style={{ width: "80%", backgroundColor: "blue" }}></TextInput>
-			<StatusBar style="auto" />
-		</View>
+		// <NavigationContainer>
+		//   <Stack.Navigator>
+		//     <Stack.Screen name="Home" component={HomeScreen} />
+		//   </Stack.Navigator>
+		// </NavigationContainer>
+		<NavigationContainer>
+			<View style={styles.container}>
+				<Text style={styles.main}>P-3 Project {":)"}</Text>
+				<TouchableOpacity>
+					<Text>안뇽</Text>
+				</TouchableOpacity>
+				<TextInput
+					style={{ width: "80%", backgroundColor: "blue" }}
+				></TextInput>
+				<Stack.Navigator>
+					<Stack.Screen name="Login" component={Login} />
+				</Stack.Navigator>
+				<StatusBar style="auto" />
+			</View>
+		</NavigationContainer>
 	);
 }
 
