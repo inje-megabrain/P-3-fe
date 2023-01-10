b8b7bb373380bb67fa9c73811e2cdb28eba6b14e
narinn-star <skfls2618@naver.com>
2023-01-10 17:23:59 +0900

    feat : (#1) authState

--- App.tsx
+++ App.tsx
@@ -5,26 +5,29 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./src/components/Login";
 import Home from "./src/components/Home";
 import { RootStackParamList } from "./src/components/types";
+import { RecoilRoot } from "recoil";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen
-					name="Home"
-					component={Home}
-					options={{ title: "Home" }}
-				/>
-				<Stack.Screen
-					name="Login"
-					component={Login}
-					initialParams={{ name: "Login", userId: "User" }}
-					options={{ title: "로그인" }}
-				/>
-			</Stack.Navigator>
-			<StatusBar style="auto" />
-		</NavigationContainer>
+		<RecoilRoot>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="Home">
+					<Stack.Screen
+						name="Home"
+						component={Home}
+						options={{ title: "Home" }}
+					/>
+					<Stack.Screen
+						name="Login"
+						component={Login}
+						initialParams={{ name: "Login", userId: "User" }}
+						options={{ title: "로그인" }}
+					/>
+				</Stack.Navigator>
+				<StatusBar style="auto" />
+			</NavigationContainer>
+		</RecoilRoot>
 	);
 }
